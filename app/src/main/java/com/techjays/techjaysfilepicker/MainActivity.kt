efd0cd884dab2e4bb2b9391071d26a50409b15ad
SrinathDev <srinath.a@techjays.com>
2022-02-21 12:19:33 +0530

    Library setup.

--- app/src/main/java/com/techjays/techjaysfilepicker/MainActivity.kt
+++ app/src/main/java/com/techjays/techjaysfilepicker/MainActivity.kt
@@ -2,6 +2,8 @@ package com.techjays.techjaysfilepicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.techjays.filepicker.FilePickActivity
+import com.techjays.filepicker.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ filepicker/src/main/java/com/techjays/filepicker/FilePickActivity.kt
@@ -0,0 +1,15 @@
+package com.techjays.filepicker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class FilePickActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_pick)
+
+    }
+
+
+}
